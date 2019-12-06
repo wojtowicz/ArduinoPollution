3c05342c7dc4a89e099cdf68dbff3091c0cbea1b
Tomasz Wójtowicz <tomash2310w@gmail.com>
2019-12-06 10:08:10 +0100

    Get pollition from server and display on LCD

--- ArduinoPollution.ino
+++ ArduinoPollution.ino
@@ -4,6 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 
 #include "WifiNetworks.h"
 
@@ -11,7 +12,12 @@
 #define APSSID "SmartHome"
 #endif
 
+#define LED_RED D5
+#define LED_YELLOW D6
+#define LED_GREEN D7
+
 const char *apssid = APSSID;
+const String arduinoApiHost = "https://tomash-arduino-api.herokuapp.com";
 
 LiquidCrystal_PCF8574 lcd(0x27);
 ESP8266WebServer server(80);
@@ -22,6 +28,8 @@ String password;
 boolean connectWifi = false;
 boolean wifiConnecting = false;
 static uint32_t timer;
+static uint32_t timer2;
+int a = 1;
 
 void handleRoot() {
   server.send(200, "text/html", wifiNetworks.renderFormHtml());
@@ -110,6 +118,13 @@ void setup() {
   delay(1000);
   Serial.begin(9600);
 
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+
   int error;
   
   Serial.println("Configuring LCD...");
@@ -162,5 +177,73 @@ void loop() {
 
   if(wifiConnecting){
     handleWifiConnection();
+  }
+
+  if(WiFi.status() == WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
+
+    if (millis() > timer) {
+      timer = millis() + 5000;
+      Serial.println("[HTTP] begin...");
+      if (http.begin(client, "http://tomash-arduino-api.herokuapp.com/pollution/measurements?lat=50.00983&lng=19.97484&fields[]=airly_caqi&fields[]=pm25&fields[]=pm10")) {
+        Serial.print("[HTTP] GET...\n");
+        int httpCode = http.GET();
+        if (httpCode > 0) {
+          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String payload = http.getString();
+            int index;
+            index = payload.indexOf("DATA:");
+            String newString = payload.substring(index + 5);
+            
+            index = newString.indexOf("|");
+            String level = newString.substring(0, index);
+            Serial.println("Level:" + level);
+            newString = newString.substring(level.length() + 1);
+          
+            index = newString.indexOf("|");
+            String pm25 = newString.substring(0, index);
+            Serial.println("PM25:" + pm25);
+            newString = newString.substring(pm25.length() + 1);
+          
+            index = newString.indexOf("|");
+            String pm10 = newString.substring(0, index);
+            Serial.println("PM10:" + pm10);
+            newString = newString.substring(pm10.length() + 1);
+            Serial.println(payload);
+
+            lcd.begin(16, 2);
+            lcd.setCursor(0, 0);
+            lcd.print("PM2.5: " + pm25 + "%");
+            lcd.setCursor(0, 1);
+            lcd.print("PM10: " + pm10 + "%");
+
+            digitalWrite(LED_RED, LOW);
+            digitalWrite(LED_YELLOW, LOW);
+            digitalWrite(LED_GREEN, LOW);
+            
+            if(level == "1"){
+              Serial.println("Level 1");
+              digitalWrite(LED_GREEN, HIGH);
+            }
+            if(level == "2"){
+              Serial.println("Level 2");
+              digitalWrite(LED_YELLOW, HIGH);
+            }
+            if(level == "3"){
+              Serial.println("Level 3");
+              digitalWrite(LED_RED, HIGH);
+            }
+          }
+        } else {
+          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+  
+        http.end();
+      } else {
+        Serial.printf("[HTTP} Unable to connect\n");
+      }
+    }
   }
 }
